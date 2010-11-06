37a0705f0c7b4691c1ff3bf2c9a8439a61d262c7
Anthony Scopatz <scopatz@gmail.com>
2010-11-06 17:09:59 -0500

    Added framework for table slicing from command line.

--- Data/data_gen.py
+++ Data/data_gen.py
@@ -2,17 +2,22 @@
 from optparse import OptionParser
 
 from facility_info import facility_info
-from used_fuel import make_used_fuel_tables
+from used_fuel import make_used_fuel_tables, calc_used_fuel_rows
 from fresh_fuel_info import make_reactor_groups, make_fresh_fuel_info
 
 def main():
     usage = "usage: %prog [options] [filename]"
     parser = OptionParser(usage=usage)
-    parser.add_option("-r", "--reactor-list",
+    parser.add_option("-r", 
                   dest="reactor_list", default=None,
                   help="Specify which reactors to do the following actions for. " + \
                        "A list of whitespace spearated values. " + \
                        "If not given, all reactors are included.")
+    parser.add_option("-s",
+                  dest="slice", default=None,
+                  help="Specify the rows for which to do the following calculation. " + \
+                       "Uses a list or tuple to specify the slice: " + \
+                       "e.g. '[1, -1, 100]' or '(50, 100)'.")
     parser.add_option("-f", "--facility-info",
                   action="store_true", dest="fac_info", default=False,
                   help="Generate the facility info table.")
@@ -25,6 +30,9 @@ def main():
     parser.add_option("-u", "--used-fuel",
                   action="store_true", dest="used_fuel", default=False,
                   help="Generates new used fuel tables. Erases current data.")
+    parser.add_option("-c", "--calc-used-fuel",
+                  action="store_true", dest="calc_used_fuel", default=False,
+                  help="Determine used fuel via a burnup calculation.")
     options, args = parser.parse_args()
 
     # set the filename
@@ -38,6 +46,12 @@ def main():
         rx_list = None
     else:
         rx_list = options.reactor_list.split()
+
+    # Set the row slice
+    if options.slice == None:
+        slice = (0, -1, 1)
+    else:
+        slice = eval(options.slice) 
 
     # Add the facility info table
     if options.fac_info:
@@ -54,6 +68,10 @@ def main():
     # Make used fuel tables
     if options.used_fuel:
         make_used_fuel_tables(filename, rx_list)
+
+    # Calculate used fuel rows
+    if options.calc_used_fuel:
+        calc_used_fuel_rows(filename, rx_list, slice)
 
 if __name__ == "__main__":
     main()
--- Data/used_fuel.py
+++ Data/used_fuel.py
@@ -60,5 +60,43 @@ def make_used_fuel_tables(hdf5_file, rx_list=None):
         hdf5_file.close()
 
 
+def calc_used_fuel_rows(hdf5_file, rx_list=None, slice=(0,-1, 1)):
+    # Open the HDF5 file
+    opened_here = False
+    if isinstance(hdf5_file, basestring):
+        opened_here = True
+        hdf5_file = tb.openFile(hdf5_file, 'a')
+
+    h5r = hdf5_file.root
+
+    # Grab reactor list if not specified
+    if rx_list == None:
+        rx_id_map = get_reactor_id_map(hdf5_file)
+        rx_list = sorted(rx_id_map.values())
+
+    # Make new used fuel tables
+    for rx in rx_list:
+        rx_group = getattr(h5r, rx)
+
+        # Determine slice for this reactor
+        if len(slice) == 1:
+            rx_slice = [slice[0], -1, 1]
+        elif len(slice) == 2:
+            rx_slice = [slice[0], slice[1], 1]
+        elif len(slice) == 3:
+            rx_slice = [slice[0], slice[1], slice[2]]
+
+        if rx_slice[1] < 0:
+            rx_slice[1] = len(rx_group.fresh_fuel_info) + rx_slice[1]
+
+        for n in xrange(*rx_slice):
+            print n
+
+    # Close the HDF5 file
+    if opened_here:
+        hdf5_file.close()
+
+
+
 if __name__ == "__main__":
     make_used_fuel_tables("test.h5")
